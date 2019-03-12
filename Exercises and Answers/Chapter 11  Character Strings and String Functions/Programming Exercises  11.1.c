f216cec7a15c1b71d512aa548becae427255ba8b
ZHU YIFAN <simba23333@gmail.com>
2019-03-12 14:30:08 +0800

    Create Programming Exercises  11.1.c
    Remember to add a '\0' to the end of the string.

---
+++ Exercises and Answers/Chapter 11  Character Strings and String Functions/Programming Exercises  11.1.c
@@ -0,0 +1,46 @@
+/* Programming Exercises 11-1 */
+
+// Design and test a function that fetches the next n characters from input (including
+// blanks, tabs, and newlines), storing the results in an array whose address is passed as an
+// argument.
+
+
+#include <stdio.h>
+#define LEN 10
+char * getnchar(char * str, int n);
+int main(void)
+{
+    char input[LEN];
+    char *check;
+
+    check = getnchar(input, LEN - 1);
+    if (check == NULL)
+        puts("Input failed.");
+    else
+        puts(input);
+    puts("Done.\n");
+
+    return 0;
+}
+
+char * getnchar(char * str, int n)
+{
+    int i;
+    int ch;
+
+    for (i = 0; i < n; i++)
+    {
+        ch = getchar();
+        if (ch != EOF)
+            str[i] = ch;
+        else
+            break;
+    }
+    if (ch == EOF)
+        return NULL;
+    else
+    {
+        str[i] = '\0';
+        return str;
+    }
+}
